749f2e8f4799af7616a580747cc71efdeec46cd8
mithoo18 <devanshu18g@gmail.com>
2023-03-31 01:47:54 +0530

    commit

---
+++ flutter_minimal_website/lib/components/spacing.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/painting.dart';
+
+const EdgeInsets marginBottom12 = EdgeInsets.only(bottom: 12);
+const EdgeInsets marginBottom24 = EdgeInsets.only(bottom: 24);
+const EdgeInsets marginBottom40 = EdgeInsets.only(bottom: 40);
