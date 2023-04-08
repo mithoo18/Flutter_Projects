7aa36b65d6c0742318b5dff6268f889458a3435a
mithoo18 <devanshu18g@gmail.com>
2023-04-08 11:03:03 +0530

    commit

--- flutter_minimal_website/lib/components/spacing.dart
+++ flutter_minimal_website/lib/components/spacing.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/painting.dart';
 
+//margin
 const EdgeInsets marginBottom12 = EdgeInsets.only(bottom: 12);
 const EdgeInsets marginBottom24 = EdgeInsets.only(bottom: 24);
 const EdgeInsets marginBottom40 = EdgeInsets.only(bottom: 40);
+
+//padding
+const EdgeInsets paddingBottom24 = EdgeInsets.only(bottom: 24);
--- flutter_minimal_website/lib/components/text.dart
+++ flutter_minimal_website/lib/components/text.dart
@@ -1,1 +1,39 @@
+import 'dart:html';
 
+import 'package:flutter/material.dart';
+import 'package:flutter_minimal_website/components/spacing.dart';
+import 'package:flutter_minimal_website/components/typography.dart';
+
+class TextBody extends StatelessWidget {
+  final String text;
+
+  const TextBody({Key? key, required this.text}) : super(key: Key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: marginBottom24,
+      child: Text(
+        text,
+        style: bodyTextStyle,
+      ),
+    );
+  }
+}
+
+class TextBodySecondary extends StatelessWidget {
+  final String text;
+
+  const TextBodySecondary({Key? key, required this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: marginBottom24,
+      child: Text(
+        text,
+        style: subtitleTextStyle,
+      ),
+    );
+  }
+}
