08bd89313eac20a5a38c385ddee19ddca69ad058
mithoo18 <devanshu18g@gmail.com>
2023-05-07 18:51:20 +0530

    changes

---
+++ pics/lib/app.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get; //We only need this function
+import 'models/image_model.dart';
+import 'dart:convert';
+import 'widgets/image_list.dart';
+
+class App extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+  int counter = 0;
+  List<ImageModel> images = [];
+
+  void fetchImage() async {
+    counter += 1;
+    var response =
+        await get('http://jsonplaceholder.typicode.com/photos/${counter}');
+    var imageModel = ImageModel.fromJson(json.decode(response.body));
+    setState(() {
+      images.add(imageModel);
+    });
+  }
+
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Lets see some images'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: fetchImage,
+          child: Icons(Icons.add),
+        ),
+        body: ImageList(images),
+      ),
+    );
+  }
+}
---
+++ pics/lib/main.dart
@@ -0,0 +1,7 @@
+//Import a helper library from Flutter to get content on the screen
+import 'package:flutter/material.dart';
+import 'package:pics/app.dart';
+
+void main() {
+  runApp(App());
+}
